1948b03885a05f63cc3d7945ce0e38048f171355
Franel <franel@debian>
2020-01-17 18:47:21 +0700

    17/01/2020 18:47

--- src/com/franel/vue/Calculette.java
+++ src/com/franel/vue/Calculette.java
@@ -9,10 +9,13 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -31,13 +34,10 @@ public class Calculette extends JFrame implements Observer{
 	
 	JButton[] tab_button = new JButton[tab_string.length];
 	
-	private JTextField ecran = new JTextField();
+	private JLabel ecran = new JLabel();
 	private Dimension dim = new Dimension(50, 40);
 	private Dimension dim2 = new Dimension(50, 31);
-	private Double chiffre1;
-	private boolean clicOperateur = false;
 	private boolean update = false;
-	private String operateur = "";
 	
 	//l'instance de notre objet controller
 	private AbstractControler controler;
@@ -52,14 +52,15 @@ public class Calculette extends JFrame implements Observer{
 		this.controler = controler;
 		this.setContentPane(container);
 		this.setVisible(true);
+		ecran.requestFocusInWindow();
 	}
 	
 	private void initComposant() {
 		Font police = new Font("Arial", Font.BOLD, 20);
-		ecran = new JTextField("");
+		ecran = new JLabel("");
 		ecran.setFont(police);
 		ecran.setHorizontalAlignment(JTextField.RIGHT);
-		ecran.setPreferredSize(new Dimension(220, 30));
+		ecran.setPreferredSize(new Dimension(220, 20));
 		
 		JPanel operateur = new JPanel();
 		operateur.setPreferredSize(new Dimension(55, 225));
@@ -100,12 +101,12 @@ public class Calculette extends JFrame implements Observer{
 				break;
 			}
 		}
+		ecran.addKeyListener(new ClavierListener());
 		panEcran.add(ecran);
-		//panEcran.setBorder(BorderFactory.createLineBorder(Color.black));
+		panEcran.setBorder(BorderFactory.createLineBorder(Color.black));
 		container.add(panEcran, BorderLayout.NORTH);
 		container.add(chiffre, BorderLayout.CENTER);
 		container.add(operateur, BorderLayout.EAST);
-		
 	}
 
 	@Override
@@ -113,6 +114,7 @@ public class Calculette extends JFrame implements Observer{
 	public void update(String str) {
 		// TODO Auto-generated method stub
 		ecran.setText(str);
+		ecran.requestFocusInWindow();
 	}
 	
 	class ChiffreListener implements ActionListener{
@@ -122,10 +124,11 @@ public class Calculette extends JFrame implements Observer{
 			// TODO Auto-generated method stub
 			//On affiche le chiffre en plus dans le label
 			String str = ((JButton)arg0.getSource()).getText();
-			if(!ecran.getText().equals("0"))
+			if(!ecran.getText().equals(""))
 				str = ecran.getText() + str;
 			
 			controler.setNombre(((JButton)arg0.getSource()).getText());
+			ecran.requestFocusInWindow();
 		}
 	}
 	
@@ -135,7 +138,7 @@ public class Calculette extends JFrame implements Observer{
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			controler.setOperateur(((JButton)e.getSource()).getText());
-			
+			ecran.requestFocusInWindow();
 		}
 	}
 	
@@ -145,6 +148,50 @@ public class Calculette extends JFrame implements Observer{
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			controler.reset();
+			ecran.requestFocusInWindow();
 		}
+	}
+	
+	class ClavierListener implements KeyListener{
+		public void keyReleased(KeyEvent event) {
+			String str = String.valueOf(event.getKeyChar());
+			String str_code = String.valueOf(event.getKeyCode());System.out.print(str_code);
+			
+			if(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/") || str.equals("=")) {
+				controler.setOperateur(str);
+		    }
+			
+			if(str_code.equals("10")) {
+				controler.setOperateur("=");
+			}
+			
+			if(str_code.equals("27")) {
+				controler.reset();
+			}
+			
+			if(str_code.equals("44") || str_code.equals("59")) {
+				controler.setNombre(".");
+			}
+			
+			if(isNumeric(str.charAt(0))) {
+				
+				controler.setNombre(str);
+			} 
+		}
+
+		//Inutile de redéfinir ces méthodes, ous n'en avons plus l'utilité !
+		public void keyPressed(KeyEvent event) {}
+		public void keyTyped(KeyEvent event) {}
+
+	    //Retourne true si le paramètre est numérique, false dans le cas contraire
+	    private boolean isNumeric(char carac){
+	    try {
+		  Integer.parseInt(String.valueOf(carac));
+		}
+		catch (NumberFormatException e) {
+		  return false;            
+		}
+		return true;
+	}
 	}
 }
